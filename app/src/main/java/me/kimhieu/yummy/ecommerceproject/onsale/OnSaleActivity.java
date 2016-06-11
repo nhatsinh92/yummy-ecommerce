21a3aad8ab5129502887f424be3cc16036188e70
sinh.huynh <nhatsinh93@gmail.com>
2016-06-11 10:34:42 +0700

    Add search wiget

--- app/src/main/java/me/kimhieu/yummy/ecommerceproject/onsale/OnSaleActivity.java
+++ app/src/main/java/me/kimhieu/yummy/ecommerceproject/onsale/OnSaleActivity.java
@@ -1,12 +1,18 @@
 package me.kimhieu.yummy.ecommerceproject.onsale;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import me.kimhieu.yummy.ecommerceproject.R;
 import me.kimhieu.yummy.ecommerceproject.model.Product;
@@ -24,10 +30,14 @@ public class OnSaleActivity extends BaseActivity {
     private OnSaleRecyclerViewAdapter recyclerViewAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
+    WooCommerceService service =  ServiceGenerator.createService(WooCommerceService.class);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_sale);
+
+        handleIntent(getIntent());
 
         // Set toolbar title
         getSupportActionBar().setTitle("On Sale");
@@ -42,19 +52,90 @@ public class OnSaleActivity extends BaseActivity {
         recyclerViewAdapter = new OnSaleRecyclerViewAdapter(this, new ArrayList<Product>());
         recyclerView.setAdapter(recyclerViewAdapter);
 
-        WooCommerceService service =  ServiceGenerator.createService(WooCommerceService.class);
+        displayOnSaleProduct();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            searchProductByNameAndDisplay(query);
+        }
+    }
+
+    private void searchProductByNameAndDisplay(final String keyWord) {
+        Call<ProductsResponse> productsResponseCall = service.getListProduct();
+        productsResponseCall.enqueue(new Callback<ProductsResponse>() {
+            @Override
+            public void onResponse(Call<ProductsResponse> call, Response<ProductsResponse> response) {
+                List<Product> productList = new ArrayList<>();
+                for (Product product: response.body().getProducts()) {
+                    if (product.getTitle().toLowerCase().contains(keyWord.toLowerCase())
+                            && product.getOnSale()
+                            ) {
+                        productList.add(product);
+                    }
+                }
+                recyclerViewAdapter.setProductList(productList);
+            }
+
+            @Override
+            public void onFailure(Call<ProductsResponse> call, Throwable t) {
+                recyclerViewAdapter.setProductList(new ArrayList<Product>());
+            }
+        });
+    }
+
+    private void displayOnSaleProduct() {
         Call<ProductsResponse> productsResponseCall = service.getListProduct();
 
         productsResponseCall.enqueue(new Callback<ProductsResponse>() {
             @Override
             public void onResponse(Call<ProductsResponse> call, Response<ProductsResponse> response) {
-                recyclerViewAdapter.setProductList(response.body().getProducts());
+                List<Product> onSaleProductList = new ArrayList<Product>();
+                for (Product product : response.body().getProducts()) {
+                    if (product.getOnSale()) {
+                        onSaleProductList.add(product);
+                    }
+                }
+                recyclerViewAdapter.setProductList(onSaleProductList);
             }
 
             @Override
             public void onFailure(Call<ProductsResponse> call, Throwable t) {
+                recyclerViewAdapter.setProductList(new ArrayList<Product>());
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                // reload data here
+                displayOnSaleProduct();
+                return false;
+            }
+        });
+
+        return true;
     }
 
 }
