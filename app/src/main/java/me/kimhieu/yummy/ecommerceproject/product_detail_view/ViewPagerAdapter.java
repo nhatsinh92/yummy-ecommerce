91d176e327de15e5c5fc5b705b7334fa9b67b9ec
baotran <baok2n@gmail.com>
2016-06-22 23:02:48 +0700

    clean up codes in view pager adapter in product detail view

--- app/src/main/java/me/kimhieu/yummy/ecommerceproject/product_detail_view/ViewPagerAdapter.java
+++ app/src/main/java/me/kimhieu/yummy/ecommerceproject/product_detail_view/ViewPagerAdapter.java
@@ -7,48 +7,6 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import java.util.List;
 
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
-//    private Context context;
-//    private List<Image> images;
-//    private List<Integer> relatedProductIds;
-//
-//    public ViewPagerAdapter(Context context, FragmentManager fm, List<Image> imagesList, List<Integer> idsList) {
-//        super(fm);
-//        this.context = context;
-//        this.images = imagesList;
-//        this.relatedProductIds = idsList;
-//    }
-//
-//    public ViewPagerAdapter(Context context, FragmentManager fm, List<Image> imagesList) {
-//        super(fm);
-//        this.context = context;
-//        this.images = imagesList;
-//    }
-//
-//    @Override
-//    public Fragment getItem(final int position) {
-//        String s = images.get(position).getSrc();
-//        int productId;
-//        try{
-//            productId = relatedProductIds.get(position);
-//        }
-//        catch (Exception e){
-//            productId = -1;
-//        }
-//
-//        Fragment f =  PageFragment.getInstance(s, productId);
-//        return f;
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return images.size();
-//    }
-//
-//    public void updatePageViewer(Image image, int id) {
-//        this.images.add(image);
-//        this.relatedProductIds.add(id);
-//        notifyDataSetChanged();
-//    }
     private List<Fragment> fragmentList;
 
     public ViewPagerAdapter(FragmentManager fm, List<Fragment> fragmentList) {
